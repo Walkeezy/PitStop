565c41d6486e73ede47ebb6ede5ec7a18f339249
michi <mikmeier@hotmail.com>
2018-10-26 15:20:56 +0200

    Added z-index for footer and vehicle name in add event view

--- pitstop/src/components/Event/AddEvent.js
+++ pitstop/src/components/Event/AddEvent.js
@@ -7,10 +7,13 @@ import EventForm from './EventForm'
 class AddVehiclePage extends Component {
 
     render() {
+        const headerTitle = this.props.vehicles.vehicles[this.props.vehicles.activeVehicle]
+            ? 'Add new event for ' + this.props.vehicles.vehicles[this.props.vehicles.activeVehicle].name
+            : 'Add new event'
         return (
 
             <div className="page">
-                <Header title="Add new event" backLink={routes.ACCOUNT} />
+                <Header title={headerTitle} backLink={routes.ACCOUNT} />
                 <div className="content-box">
                     <EventForm {...this.props} />
                 </div>
