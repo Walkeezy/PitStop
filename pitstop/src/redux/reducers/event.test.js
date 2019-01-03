f9928963ef4fe9091af4e565fd97d330839ddbaf
michi <mikmeier@hotmail.com>
2019-01-03 13:45:13 +0100

    updated eventReducer test

--- pitstop/src/redux/reducers/event.test.js
+++ pitstop/src/redux/reducers/event.test.js
@@ -4,7 +4,8 @@ describe('auth event reducer', () => {
     it('should return the initial state', () => {
         expect(eventReducer(undefined, {})).toEqual({
             loading: true,
-            events: {}
+            events: {},
+            eventsArray: []
         });
     });
 });
